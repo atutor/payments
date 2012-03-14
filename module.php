7d4d7b809cda051157eb1d84c53cef0635a15e2b
Greg <greg@atutorspaces.com>
2012-03-14 14:36:27 -0400

    change module path from old dirname ecomm, to new dirname payments

--- module.php
+++ module.php
@@ -20,59 +20,59 @@ define('AT_ADMIN_PRIV_ECOMM', $this->getAdminPrivilege());
 /*******
  * instructor Manage section:
  */
-$this->_pages['mods/ecomm/response_ipn.php']['title_var']     = 'ec_payments';
-$this->_pages['mods/ecomm/response_user.php']['title_var']    = 'ec_payments';
-$this->_pages['mods/ecomm/error_beanstream.php']['title_var']    = 'ec_payments';
-$this->_pages['mods/ecomm/success_beanstream.php']['title_var']    = 'ec_payments';
-$this->_pages['mods/ecomm/failure_beanstream.php']['title_var']    = 'ec_payments';
-$this->_pages['mods/ecomm/success_monerisusa.php']['title_var']    = 'ec_payments';
-$this->_pages['mods/ecomm/error_monerisca.php']['title_var']    = 'ec_payments';
-$this->_pages['mods/ecomm/success_monerisca.php']['title_var']    = 'ec_payments';
-$this->_pages['mods/ecomm/failure_monerisca.php']['title_var']    = 'ec_payments';
+$this->_pages['mods/payments/response_ipn.php']['title_var']     = 'ec_payments';
+$this->_pages['mods/payments/response_user.php']['title_var']    = 'ec_payments';
+$this->_pages['mods/payments/error_beanstream.php']['title_var']    = 'ec_payments';
+$this->_pages['mods/payments/success_beanstream.php']['title_var']    = 'ec_payments';
+$this->_pages['mods/payments/failure_beanstream.php']['title_var']    = 'ec_payments';
+$this->_pages['mods/payments/success_monerisusa.php']['title_var']    = 'ec_payments';
+$this->_pages['mods/payments/error_monerisca.php']['title_var']    = 'ec_payments';
+$this->_pages['mods/payments/success_monerisca.php']['title_var']    = 'ec_payments';
+$this->_pages['mods/payments/failure_monerisca.php']['title_var']    = 'ec_payments';
 
-$this->_pages['mods/ecomm/index_instructor.php']['title_var'] = 'ec_payments';
-$this->_pages['mods/ecomm/index_instructor.php']['parent']    = 'tools/index.php';
-$this->_pages['mods/ecomm/index_instructor.php']['children']  = array('mods/_core/enrolment/index.php');
-$this->_pages['tools/enrollment/index.php']['children']       = array('mods/ecomm/index_instructor.php');
+$this->_pages['mods/payments/index_instructor.php']['title_var'] = 'ec_payments';
+$this->_pages['mods/payments/index_instructor.php']['parent']    = 'tools/index.php';
+$this->_pages['mods/payments/index_instructor.php']['children']  = array('mods/_core/enrolment/index.php');
+$this->_pages['tools/enrollment/index.php']['children']       = array('mods/payments/index_instructor.php');
 
 /*******
  * add the admin pages when needed.
  */
 if (admin_authenticate(AT_ADMIN_PRIV_ECOMM, TRUE) || admin_authenticate(AT_ADMIN_PRIV_ADMIN, TRUE)) {
-	$this->_pages[AT_NAV_ADMIN] = array('mods/ecomm/payments_admin.php');
+	$this->_pages[AT_NAV_ADMIN] = array('mods/payments/payments_admin.php');
 	
-	$this->_pages['mods/ecomm/payments_admin.php']['title_var'] = 'ec_payments';
+	$this->_pages['mods/payments/payments_admin.php']['title_var'] = 'ec_payments';
 	
-	$this->_pages['mods/ecomm/payments_admin.php']['parent']    = AT_NAV_ADMIN;
+	$this->_pages['mods/payments/payments_admin.php']['parent']    = AT_NAV_ADMIN;
 
-	$this->_pages['mods/ecomm/payments_admin.php']['children'] = array('mods/ecomm/index_admin.php','mods/ecomm/index_admin_approve.php', 'mods/ecomm/index_instructor.php','mods/_core/enrolment/admin/index.php', );
+	$this->_pages['mods/payments/payments_admin.php']['children'] = array('mods/payments/index_admin.php','mods/payments/index_admin_approve.php', 'mods/payments/index_instructor.php','mods/_core/enrolment/admin/index.php', );
 	
-	$this->_pages['mods/ecomm/index_instructor.php']['title_var'] = 'ec_payments_courses';	
+	$this->_pages['mods/payments/index_instructor.php']['title_var'] = 'ec_payments_courses';	
 	
-	$this->_pages['mods/ecomm/index_instructor.php']['children']  = array('mods/_core/enrolment/admin/index.php');
+	$this->_pages['mods/payments/index_instructor.php']['children']  = array('mods/_core/enrolment/admin/index.php');
 	
-	$this->_pages['mods/ecomm/index_instructor.php']['parent']    = 'mods/ecomm/payments_admin.php';
+	$this->_pages['mods/payments/index_instructor.php']['parent']    = 'mods/payments/payments_admin.php';
 	
-	$this->_pages['mods/ecomm/index_admin.php']['title_var'] = 'ec_settings';
+	$this->_pages['mods/payments/index_admin.php']['title_var'] = 'ec_settings';
 	
-	$this->_pages['mods/ecomm/index_admin.php']['parent']    = 'mods/ecomm/payments_admin.php';
+	$this->_pages['mods/payments/index_admin.php']['parent']    = 'mods/payments/payments_admin.php';
 
-	$this->_pages['mods/_core/enrolment/admin/index.php']['children']       = array('mods/ecomm/index_instructor.php');
+	$this->_pages['mods/_core/enrolment/admin/index.php']['children']       = array('mods/payments/index_instructor.php');
 
-	$this->_pages['mods/ecomm/index_admin_approve.php']['title_var'] = 'ec_approve_manually';
+	$this->_pages['mods/payments/index_admin_approve.php']['title_var'] = 'ec_approve_manually';
 		
-	$this->_pages['mods/ecomm/index_admin_approve.php']['parent']    = 'mods/ecomm/payments_admin.php';
+	$this->_pages['mods/payments/index_admin_approve.php']['parent']    = 'mods/payments/payments_admin.php';
 }
 
 /* my start page pages */
-$this->_pages[AT_NAV_START]  = array('mods/ecomm/index.php');
-$this->_pages['mods/ecomm/index.php']['title_var'] = 'ec_payments';
-$this->_pages['mods/ecomm/index.php']['parent']    = AT_NAV_START;
+$this->_pages[AT_NAV_START]  = array('mods/payments/index.php');
+$this->_pages['mods/payments/index.php']['title_var'] = 'ec_payments';
+$this->_pages['mods/payments/index.php']['parent']    = AT_NAV_START;
 
-$this->_pages['mods/ecomm/payment.php']['title_var'] = 'ec_payments';
-$this->_pages['mods/ecomm/payment.php']['parent']    = 'mods/ecomm/index.php';
-$this->_pages['mods/ecomm/index.php']['children']    = array('users/index.php','users/browse.php');
+$this->_pages['mods/payments/payment.php']['title_var'] = 'ec_payments';
+$this->_pages['mods/payments/payment.php']['parent']    = 'mods/payments/index.php';
+$this->_pages['mods/payments/index.php']['children']    = array('users/index.php','users/browse.php');
 
-$this->_pages['mods/ecomm/failure.php']['title_var'] = 'ec_payments';
-$this->_pages['mods/ecomm/invoice.php']['title_var'] = 'ec_payments';
+$this->_pages['mods/payments/failure.php']['title_var'] = 'ec_payments';
+$this->_pages['mods/payments/invoice.php']['title_var'] = 'ec_payments';
 ?>
